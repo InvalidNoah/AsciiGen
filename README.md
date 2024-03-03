23e723d5d6e04ae539c3390947eed1651131d90d
Noah <info.famegames@gmail.com>
2024-03-03 15:03:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# ASCII Generator
+
+## Installing
+```shell
+composer require austriannoah/asciigenerator
+```
+
+or just adding this into your ``composer.json`` file:
+```json
+{
+  "require": {
+    "austriannoah/asciigenerator": "dev-main"
+  }
+}
+```
+
+## Support
+
+you can contact me over Discord: `eynoah`
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'austriannoah/asciigenerator',
+        'pretty_version' => 'v1.0.0',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'austriannoah/asciigenerator' => array(
+            'pretty_version' => 'v1.0.0',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
